a8e83f3979c58fde7a98611009c043abfc23c4d3
eh04987 <eh04987@CEIT2553214X020.ad.georgiasouthern.edu>
2019-08-21 17:04:03 -0400

    Asking User for name and saying name back to them

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -6,6 +6,12 @@ public class FirstClassInGit {
 	public static void main(String[] args) {
 		System.out.println("Hello my name is Eli!");
 		
+		System.out.println("What is your name?");
+		Scanner input = new Scanner(System.in);
+		String name = input.nextLine();
+		
+		System.out.println("Your name is " + name);
+		
 		//add a statement to display your name in the console
 		
 		//Then commit your changes
