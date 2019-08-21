6a8ec741ae26c96bd78b003837ae654e565fd563
eh04987 <eh04987@CEIT2553214X020.ad.georgiasouthern.edu>
2019-08-21 16:44:58 -0400

    Message

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,9 +1,10 @@
-package assignment;
 
+package assignment;
+import java.util.Scanner;
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello MY name is Eli!");
+		System.out.println("Hello MY nAme is Eli!");
 		
 		//add a statement to display your name in the console
 		
